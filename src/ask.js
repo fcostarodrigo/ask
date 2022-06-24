31cad3efe44047399b3c30af5e997f6ba34357b7
Rodrigo Fernandes da Costa <--local>
2022-06-24 08:52:47 +0100

    Improve autocomplete suggestions and fix required boolean

--- src/ask.js
+++ src/ask.js
@@ -1,11 +1,24 @@
 import {sentenceCase} from 'change-case';
 import enquirer from 'enquirer';
+import compareStrings from 'compare-strings';
+
+function suggest(input, choices) {
+	const scores = new Map();
+
+	for (const choice of choices) {
+		scores.set(choice, compareStrings(input, choice.name));
+	}
+
+	return choices
+		.filter((choice) => scores.get(choice) > 0.5)
+		.sort((a, b) => scores.get(b) - scores.get(a));
+}
 
 const typeMap = {
-	list: ({options}) => (options ? {type: 'autocomplete', multiple: true} : {type: 'list'}),
+	list: ({options}) => (options ? {type: 'autocomplete', multiple: true, suggest} : {type: 'list'}),
 	boolean: () => ({type: 'confirm'}),
 	number: () => ({type: 'numeral'}),
-	string: ({options}) => ({type: options ? 'autocomplete' : 'input'}),
+	string: ({options}) => (options ? {type: 'autocomplete', suggest} : {type: 'input'}),
 	password: () => ({type: 'invisible'}),
 };
 
